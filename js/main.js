fc5e7bb29bd243787825c1ac744f3f9db267d5e2
Carlos Giammaria <carlosgiammaria29@gmail.com>
2022-12-14 10:11:27 -0500

    Added functions active, all, complete and clear complete

--- js/main.js
+++ js/main.js
@@ -1,37 +1,130 @@
 const $form = document.querySelector(".js-submit");
-const lista = document.querySelector(".js-lista");
+const list = document.querySelector(".js-list");
+const counter = document.querySelector(".js-counter");
+const clearList = document.querySelector(".js-clear-task");
+const btnComplete = document.querySelector(".js-task-complete");
+const btnAll = document.querySelector(".js-btn-all");
+const btnActive = document.querySelector(".js-btn-active");
+const btnCheck = document.querySelector(".js-btn-check");
+let items = [];
+
+const ACTIVE_TASK = "ACTIVE_TASK";
+const COMPLETE_TASK = "COMPLETE_TASK";
+const ALL_TASK = "ALL_TASK";
 
 $form.addEventListener("submit", function (ev) {
   ev.preventDefault();
   const formData = new FormData(ev.target);
   const text = formData.get("text");
-  const template = `
-    <li class="list__item">
-      <label class="checkbox">
-        <input
-          class="checkbox__input"
-          type="checkbox"  
-          name="myCheckboxName"
-        />
-        <div class="checkbox__box"></div>
-        <span>${text}</span>
-      </label>
-      <button class="checkbox__button js-btn-eliminar">
-              <img src="/images/icon-delete.svg"    alt="icon-delete"/>
-         </button>
-    </li>
-  `
+  const check = formData.get("checkbox");
+  const isChecked = Boolean(check);
+  items.push({ id: items.length + 1, text, checked: isChecked });
+
+  const template = renderTemplate({
+    id: items.length,
+    text,
+    checked: isChecked,
+  });
+
+  list.insertAdjacentHTML("afterbegin", template);
+  renderOrUpdateItemsLeftlElement(list.children.length);
   $form.reset();
-  lista.insertAdjacentHTML("afterbegin", template);
+});
+
+//taskComplete
+btnComplete.addEventListener("click", (e) => {
+  e.preventDefault();
+  obtenerTarea(COMPLETE_TASK);
+  console.log(items);
+});
+
+btnActive.addEventListener("click", (e) => {
+  e.preventDefault();
+  obtenerTarea(ACTIVE_TASK);
 });
 
 
-lista.addEventListener("click", (ev) => {
-  const target = ev.target
-   console.log('target', target) 
-  if (target.classList.contains('js-btn-eliminar')) {
-    const parent = target.closest('.list__item')
-     console.log('entro', parent) 
+//All
+btnAll.addEventListener("click", (e) => {
+  e.preventDefault();
+  obtenerTarea(ALL_TASK);
+});
+
+function obtenerTarea(action) {
+  list.innerHTML = "";
+  items.forEach(({ id, text, checked }) => {
+    let template = "";
+
+    if (action === COMPLETE_TASK && checked) {
+      template = renderTemplate({ id, text, checked });
+    }
+
+    if (action === ALL_TASK) {
+      template = renderTemplate({ id, text, checked });
+    }
+
+    if (action == ACTIVE_TASK && !checked) {
+      template = renderTemplate({ id, text, checked });
+    }
+
+    if (template) {
+      list.insertAdjacentHTML("afterbegin", template);
+    }
+  });
+  renderOrUpdateItemsLeftlElement(list.children.length);
+}
+
+//Clear
+clearList.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  list.innerHTML = "";
+  renderOrUpdateItemsLeftlElement(0);
+});
+
+//Delete
+list.addEventListener("click", (ev) => {
+  const target = ev.target;
+  console.log(target)
+  if (target.classList.contains("js-btn-eliminar")) {
+    const parent = target.closest(".list__item");
     parent.remove(target);
+    renderOrUpdateItemsLeftlElement(list.children.length);
+  }
+  if (target.classList.contains("js-btn-check")) {
+    const id = parseInt(target.value);
+
+    console.log("target", target);
+
+    items.forEach((item) => {
+      if (item.id === id) {
+        item.checked = target.checked;
+      }
+      return item;
+    });
   }
 });
+
+//Counter
+function renderOrUpdateItemsLeftlElement(num = 0) {
+  counter.textContent = `${num} items left`;
+}
+
+function renderTemplate({ id, text, checked }) {
+  return `
+     <li class="list__item">
+       <label class="checkbox">
+         <input
+            value="${id}"
+           class="checkbox__input js-btn-check"
+           type="checkbox"  
+           ${checked ? "checked" : ""}
+         />
+         <div class="checkbox__box"></div>
+         <span>${text}</span>
+       </label>
+       <button class="checkbox__button js-btn-eliminar">
+         <img src="/images/icon-delete.svg" alt="icon-delete"/>
+       </button>
+     </li>
+   `;
+}
